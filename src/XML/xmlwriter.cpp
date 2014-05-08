0efad9e4cd691fdef53a7584f64a10899fc07637
Tudor Marcu <tudormarcu4@gmail.com>
2014-05-08 12:24:16 -0700

    Update xmlwriter.

--- src/XML/xmlwriter.cpp
+++ src/XML/xmlwriter.cpp
@@ -1,24 +1,8 @@
+#include <iomanip>
+
 #include "xmlwriter.h"
 
 using namespace std;
-
-static void write_cpu(ofstream *output, request *rq)
-{
-	*output << "<CPU>\n" \
-	<< "\t<Speed>" << rq->data.cpu.speed << "</Speed>\n" \
-	<< "\t<CPUID>" << rq->data.cpu.cpu_id << "</CPUID>\n" \
-	<< "\t<Generation>" << rq->data.cpu.generation << "</Generation>\n" \
-	<< "</CPU>" << endl;
-}
-
-static void write_ram(ofstream *output, request *rq)
-{
-	*output << "<RAM>\n" \
-	<< "\t<Speed>" << rq->data.ram.speed << "</Speed>\n" \
-	<< "\t<Type>" << rq->data.ram.type << "</Type>\n" \
-	<< "\t<Size>" << rq->data.ram.size << "</Size>\n" \
-	<< "</RAM>" << endl;
-}
 
 void XMLWriter::write_tag(const std::string format, const std::string tag)
 {
@@ -38,39 +22,21 @@ static void write_instruction(ofstream *output, request *rq)
 	*output << "</Instruction>" <<endl;
 }
 
-/*
-struct cacheinfo {
-	char *type;
-	int loadhits;
-	int loadmisses;
-	int loadaccess;
-	int loadmissrate;
-	int storehits;
-	int storemisses;
-	int storeaccess;
-	int storemissrate;
-	int totalhit;
-	int totalmiss;
-	int totalaccess;
-	int totalmissrate;
-};
-*/
-
 static void write_cache(ofstream *output, request *rq)
 {
-	*output << "\t\t<Cache name=\"" << rq->data.cache.type << "\">\n" \
+	*output << std::fixed << "\t\t<Cache name=\"" << rq->data.cache.type << "\">\n" \
 	<< "\t\t\t<LoadHits>"		<< rq->data.cache.loadhits << "</LoadHits>\n" \
 	<< "\t\t\t<LoadMisses>"		<< rq->data.cache.loadmisses << "</LoadMisses>\n" \
 	<< "\t\t\t<LoadAccesses>"	<< rq->data.cache.loadaccess << "</LoadAccesses>\n" \
-	<< "\t\t\t<LoadMissRate>"	<< rq->data.cache.loadmissrate << "</LoadMissRate>\n" \
+	<< "\t\t\t<LoadMissRate>"	<< std::setprecision(3) << rq->data.cache.loadmissrate << "%</LoadMissRate>\n" \
 	<< "\t\t\t<StoreHits>"		<< rq->data.cache.storehits << "</StoreHits>\n" \
 	<< "\t\t\t<StoreMisses>"	<< rq->data.cache.storemisses << "</StoreMisses>\n" \
 	<< "\t\t\t<StoreAccesses>"	<< rq->data.cache.storeaccess << "</StoreAccesses>\n" \
-	<< "\t\t\t<StoreMissRate>"	<< rq->data.cache.storemissrate << "</StoreMissRate>\n" \
+	<< "\t\t\t<StoreMissRate>"	<< rq->data.cache.storemissrate << "%</StoreMissRate>\n" \
 	<< "\t\t\t<TotalHits>"		<< rq->data.cache.storehits << "</TotalHits>\n" \
 	<< "\t\t\t<TotalMisses>"	<< rq->data.cache.storemisses << "</TotalMisses>\n" \
 	<< "\t\t\t<TotalAccesses>"	<< rq->data.cache.storeaccess << "</TotalAccesses>\n" \
-	<< "\t\t\t<TotalMissRate>"	<< rq->data.cache.storemissrate << "</TotalMissRate>\n" \
+	<< "\t\t\t<TotalMissRate>"	<< rq->data.cache.storemissrate << "%</TotalMissRate>\n" \
 	<< "\t\t</Cache>" << endl;
 }
 
@@ -105,12 +71,6 @@ void XMLWriter::create_file(const char *filename)
 void XMLWriter::write_request(request *rq)
 {
 	switch (rq->type) {
-	case 'p':
-		write_cpu(&this->datafile, rq);
-	break;
-	case 'r':
-		write_ram(&this->datafile, rq);
-	break;
 	case 'o':
 		write_instruction(&this->datafile, rq);
 	break;
